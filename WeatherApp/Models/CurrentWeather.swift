8ada0cd440a476d751e2518f593e90541f5abaa6
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2023-07-26 21:32:57 +0200

    final version

---
+++ WeatherApp/Models/CurrentWeather.swift
@@ -0,0 +1,8 @@
+//
+//  CurrentWeather.swift
+//  WeatherApp
+//
+//  Created by Maryna Bolotska on 26/07/23.
+//
+
+import Foundation
---
+++ WeatherApp/Models/CurrentWeatherData.swift
@@ -0,0 +1,8 @@
+//
+//  CurrentWeatherData.swift
+//  WeatherApp
+//
+//  Created by Maryna Bolotska on 25/07/23.
+//
+
+import Foundation
